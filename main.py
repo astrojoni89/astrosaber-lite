861c11fa7f2718fcd6f3a51d6503624dacd03f2e
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2024-01-21 04:25:51 +0100

    Aktualisieren von main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from PySide6.QtWidgets import QApplication
 from astrosaber_widget import mainWidget
 import sys
-import qdarktheme
+import qdarktheme #optional
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     # set up theme
-    qdarktheme.setup_theme()
+    qdarktheme.setup_theme() #optional
 
     widget = mainWidget(app)
     widget.show()
